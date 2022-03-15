11e602de83033cd73ef3fbf7046a1e9fd891d4ca
yuhaiyang01 <93248087+yuhaiyang01@users.noreply.github.com>
2022-03-15 15:07:37 +0800

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,44 @@
+{
+name: "test10",
+logoURI: "https://coin.top/production/logo/usdj.png",
+timestamp: 1610520957490,
+tokens: [
+{
+chainId: 1,
+address: "TEoh646E7wMrb1ksBqLkMT7Z6ooRzTMxMH",
+name: "TronixTest",
+symbol: "TronixTest",
+decimals: 6,
+logoURI: "https://coin.top/production/upload/logo/TL6K6iaEkn8kdnJ79a8Be3S4RFf4pFkGE8.png"
+},
+{
+chainId: 1,
+address: "TMcKBpKreFgHAirwo93AfTMQKKApq4BhNf",
+name: "Trlm",
+symbol: "TronixTestlm",
+decimals: 6,
+logoURI: "https://coin.top/production/upload/logo/TL6K6iaEkn8kdnJ79a8Be3S4RFf4pFkGE8.png"
+},
+{
+chainId: 1,
+address: "TLBaRhANQoJFTqre9Nf1mjuwNWjCJeYqUL",
+name: "USDJ",
+symbol: "USDJ",
+decimals: 18,
+logoURI: "https://coin.top/production/logo/usdj.png"
+},
+{
+chainId: 1,
+address: "TLBaRhANQoJFTqre9Nf1mjuwNWjCJeYqUL",
+name: "USDJ",
+symbol: "USDJ",
+decimals: 16,
+logoURI: "https://coin.top/production/logo/usdj.png"
+}
+],
+version: {
+major: 1,
+minor: 1,
+patch: 0
+}
+}
